2badbcd54809dd0bd5ca56597204e50c88d6a487
DiscowZombie <mcdiscozombie@gmail.com>
2018-01-02 21:14:21 +0100

    Fonction pour dessiner le menu

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
+"""On déssine la belle apparition du logo"""
+def drawstarting():
+    #Le bel effet d'apparition
+    for i in range(1, 101):
+        Surface = pygame.Surface((200,200)) # la surface ou on va mettre le titre du jeu et l'image du jeu
+        Surface.fill((255,255,255))
+        Surface.set_alpha(int(2.55*i)) # alpha finale: 255 (opaque)
+
+        Texte = pygame.font.SysFont("arial", int(0.44*i)) # taille finale du font: 44
+        SurfaceTexte = Texte.render("Mad Runner", True, (0,0,0))
+        Surface.blit(SurfaceTexte,(100 - i,0)) # position finale: 0 sur 0
+
+        Icone = pygame.transform.scale(ImageMenu,(2*i,int(1.2*i))) # taille finale: 200 sur 120
+        Surface.blit(Icone,(100 - i,140 - int(0.6*i))) # position finale: 0 sur 80
+
+        screen.blit(Surface,(220,120)) # position finale: 220 sur 120
+        pygame.display.flip() # acutalise ce qui doit etre affichee
+
 import pygame
 
 #On itialise le module
@@ -14,29 +32,7 @@ pygame.display.set_icon(ImageMenu) # Icone du jeu
 #On charge l'horloge de pygame
 clock = pygame.time.Clock()
 
-#Le bel effet d'apparition
-for i in range(1, 101):
-    """on peut utiliser ces 3 lignes pour completement renouveler l'ecran, mais je trouve que ca donnait un resultat moins beau, tu peux essayer"""
-    #a = pygame.Surface((640,480)) # une surface
-    #a.fill((255,255,255))
-    #fenetre.blit(a,(0,0)) # position finale: 220 sur 100
-
-    Surface = pygame.Surface((200,200)) # la surface ou on va mettre le titre du jeu et l'image du jeu
-    Surface.fill((255,255,255))
-    Surface.set_alpha(int(2.55*i)) # alpha finale: 255 (opaque)
-
-    Texte = pygame.font.SysFont("arial", int(0.44*i)) # taille finale du font: 44
-    SurfaceTexte = Texte.render("Mad Runner", True, (0,0,0))
-    Surface.blit(SurfaceTexte,(100 - i,0)) # position finale: 0 sur 0
-
-    Icone = pygame.transform.scale(ImageMenu,(2*i,int(1.2*i))) # taille finale: 200 sur 120
-    Surface.blit(Icone,(100 - i,140 - int(0.6*i))) # position finale: 0 sur 80
-
-    screen.blit(Surface,(220,120)) # position finale: 220 sur 120
-    pygame.display.flip() # acutalise ce qui doit etre affichee
-
-    #time.sleep(0.1) # j'utilisait ca pour voir si l'animation etait assez fluide ou pas
-
+drawstarting()
 
 run = True
 
