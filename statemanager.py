79f00a04d1951c6e16378c316e5aae8c2bf3ad5a
DiscowZombie <mcdiscozombie@gmail.com>
2018-01-05 18:09:58 +0100

    Gestionnaire de Status

---
+++ statemanager.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class StateEnum(Enum):
+    """
+    Une énumration des états de jeu
+    """
+    MAIN_MENU = "",
+    STATS_MENU = "",
+    SETTINGS_MENU = "",
+    PLAYING = ""
+
+class StateManager:
+
+    laststate = None #None = Null (on peut utiliser if... is None)
+
+    """
+    Change l'état de jeu
+    <p>
+    :param state - L'état de jeu (un StateEnum ou None)
+    """
+    @staticmethod
+    def setstate(state):
+        StateManager.laststate = state
+
+    @staticmethod
+    def getstate():
+        return StateManager.laststate
+
