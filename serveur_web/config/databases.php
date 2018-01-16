d0ed0bf312b222bd678a21e9b42a75c367cf5729
DiscowZombie <mcdiscozombie@gmail.com>
2018-01-16 19:22:44 +0100

    Recup des config de BDD en .json plutot qu'en clair ! J'amais de MDP en clair git status

--- serveur_web/config/databases.php
+++ serveur_web/config/databases.php
@@ -1,10 +1,12 @@
 <?php
 
-define("SGBD", "mysql");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DATABASE", "madrunner");
+$conf_jsonfile = json_decode(file_get_contents("config/config.json"));
+
+define("SGBD", $conf_jsonfile->SGBD);
+define("DB_HOST", $conf_jsonfile->DB_HOST);
+define("DB_USER", $conf_jsonfile->DB_USER);
+define("DB_PASSWORD", $conf_jsonfile->DB_PASSWORD);
+define("DATABASE", $conf_jsonfile->DATABASE);
 
 try {
 	$pdo = new PDO(SGBD.":host=".DB_HOST.";dbname=".DATABASE, DB_USER, DB_PASSWORD);
