3f39752472a966f169c9fc77fc567b12d1375a22
DiscowZombie <mcdiscozombie@gmail.com>
2018-03-05 12:25:59 +0100

    Changement de couleur de la barre d'energie, preparation de la fin du jeu en 400m + 400m haie

--- jeu_python/coregame/coregame.py
+++ jeu_python/coregame/coregame.py
@@ -19,12 +19,12 @@ Ne fonctionne pour piur 60 fps pour le moment
 
 
 class Character:
-
     speed = 0
 
     def __init__(self, characterfeatures, characterinfos, posx, posy, scalex, scaley):
         for spritename in characterinfos:
-            self.__setattr__(spritename + "sprite", sprit.SpriteStripAnim(characterinfos[spritename], posx, posy, scalex, scaley))
+            self.__setattr__(spritename + "sprite",
+                             sprit.SpriteStripAnim(characterinfos[spritename], posx, posy, scalex, scaley))
         self.characterinfos = characterinfos
         self.y = 100  # position arbitraire pour l'instant, permet de dessiner le personnage dans un axe y correct (en effet, quand le personnage saute, sa position dans l'axe y va changer !)
         self.state = "run"
@@ -40,13 +40,12 @@ class Character:
         if not self.jumping and self.energy >= 10:  # unité encore arbitraire pour l'énergie, on verra cela plus tard !
             self.energy -= 10
             # Sauter reduit sa vitesse de 15%
-            self.speed -= (15/100) * self.speed
+            self.speed -= (15 / 100) * self.speed
             self.jumping = True
             self.running = False
 
 
 class CoreGame:
-
     characters_sprite = []
     carte = None
     modejeu = None
@@ -60,7 +59,10 @@ class CoreGame:
     vitesseobj = None  # le texte sur lequel on écrit la vitesse du courreur
     tempsobj = None  # le texte sur lequel on écrit le temps de la course (AFFICHé SEULEMENT EN 400M ET 400M HAIE)
     distanceobj = None  # le texte sur lequel on écrit la distance parcouru (AFFICHé SEULEMENT EN COURSE INFINI)
-    barre_energie_in = None  # la barre d'energie
+
+    # La barre d'énergie
+    barre_energie_in = None
+    barre_energie_out = None
 
     def __init__(self, carte, modejeu, level):
         """
@@ -98,9 +100,10 @@ class CoreGame:
         ALPHA = 255  # opaque
         CONVERT_ALPHA = False
 
-        barre_energie_out = surface.Surface(ALPHA, CONVERT_ALPHA, v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
-                                    HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
-                                    BORDURE)
+        CoreGame.barre_energie_out = surface.Surface(ALPHA, CONVERT_ALPHA, v.View.screen, POSITION_X, POSITION_Y,
+                                                     SCALE_X, SCALE_Y, LARGEUR,
+                                                     HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
+                                                     BORDURE)
 
         # l'intérieur (en tant qu'objet rect, et non en tant qu'objet surface)
         LARGEUR = -4
@@ -111,12 +114,12 @@ class CoreGame:
         SCALE_Y = 0
         SCALE_WIDTH = 1
         SCALE_HEIGHT = 1
-        COULEUR = constantes.YELLOW
+        COULEUR = constantes.GREEN
         BORDURE = 0  # rempli
 
-        CoreGame.barre_energie_in = rect.Rect(barre_energie_out, POSITION_X, POSITION_Y, SCALE_X,
-                                        SCALE_Y, LARGEUR, HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
-                                        BORDURE)
+        CoreGame.barre_energie_in = rect.Rect(CoreGame.barre_energie_out, POSITION_X, POSITION_Y, SCALE_X,
+                                              SCALE_Y, LARGEUR, HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
+                                              BORDURE)
 
         # Création du texte qui affiche la vitesse
         TEXTE = ""
@@ -140,9 +143,10 @@ class CoreGame:
         COULEUR_ARRIERE = constantes.WHITE
         BORDURE = 0
 
-        CoreGame.vitesseobj = text.Text(TEXTE, ANTIALIAS, COULEUR, FONT, TAILLE_FONT, CENTRE_X, CENTRE_Y, ARRIERE_PLAN, ECART, SEUL,
-                           v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
-                           HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR_ARRIERE, BORDURE)
+        CoreGame.vitesseobj = text.Text(TEXTE, ANTIALIAS, COULEUR, FONT, TAILLE_FONT, CENTRE_X, CENTRE_Y, ARRIERE_PLAN,
+                                        ECART, SEUL,
+                                        v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
+                                        HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR_ARRIERE, BORDURE)
 
         # Création du texte soit pour la distance (course infinie), soit pour le temps (400m et 400m haie)
         TEXTE = ""
@@ -167,9 +171,10 @@ class CoreGame:
         BORDURE = 0
 
         if modejeu == "400m":
-            CoreGame.tempsobj = text.Text(TEXTE, ANTIALIAS, COULEUR, FONT, TAILLE_FONT, CENTRE_X, CENTRE_Y, ARRIERE_PLAN, ECART, SEUL,
-                           v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
-                           HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR_ARRIERE, BORDURE)
+            CoreGame.tempsobj = text.Text(TEXTE, ANTIALIAS, COULEUR, FONT, TAILLE_FONT, CENTRE_X, CENTRE_Y,
+                                          ARRIERE_PLAN, ECART, SEUL,
+                                          v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
+                                          HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR_ARRIERE, BORDURE)
 
         # Création du bouton pause
         POSITION_X = 0
@@ -192,9 +197,10 @@ class CoreGame:
         ECART = 0
         BORDURE = 3
 
-        button.BPause("| |", ANTIALIAS, COULEUR_TEXTE, ARRIERE_PLAN_TEXTE, FONT, TAILLE_FONT, CENTRE_X, CENTRE_Y, ARRIERE_PLAN,
-                ECART, v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
-                HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR, BORDURE)
+        button.BPause("| |", ANTIALIAS, COULEUR_TEXTE, ARRIERE_PLAN_TEXTE, FONT, TAILLE_FONT, CENTRE_X, CENTRE_Y,
+                      ARRIERE_PLAN,
+                      ECART, v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
+                      HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR, BORDURE)
 
         # Chargement de la piste
         # Mis en place de la piste
@@ -226,9 +232,10 @@ class CoreGame:
         ALPHA = 255  # opque
         CONVERT_ALPHA = False
 
-        self.surface_boutons = surface.Surface(ALPHA, CONVERT_ALPHA, v.View.screen, POSITION_X, POSITION_Y, SCALE_X, SCALE_Y, LARGEUR,
-                                HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
-                                BORDURE)
+        self.surface_boutons = surface.Surface(ALPHA, CONVERT_ALPHA, v.View.screen, POSITION_X, POSITION_Y, SCALE_X,
+                                               SCALE_Y, LARGEUR,
+                                               HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
+                                               BORDURE)
 
         CoreGame.surface_boutons = self.surface_boutons
 
@@ -243,29 +250,58 @@ class CoreGame:
         SCALE_X = 0.85
         SCALE_Y = 0.35
 
-        CoreGame.characters_sprite.append(Character(constantes.CharactersFeatures["gros"], constantes.Animations["gros"], POSITION_X, POSITION_Y, SCALE_X, SCALE_Y))  # plus tard dans le développement du jeu, on devra  selectionner le sprite qui convient !
+        CoreGame.characters_sprite.append(
+            Character(constantes.CharactersFeatures["gros"], constantes.Animations["gros"], POSITION_X, POSITION_Y,
+                      SCALE_X,
+                      SCALE_Y))  # plus tard dans le développement du jeu, on devra  selectionner le sprite qui convient !
 
     def loop(cls, passed=0):  # update l'arrière plan + chaque personnage
 
         if not CoreGame.pause:
             charspeed = CoreGame.characters_sprite[0].speed  # ATENTION: NE MARCHE QU'EN MODE 1 JOUEUR !!!
             d1 = CoreGame.distance
-            d2 = d1 + charspeed*(passed/1000)
+            d2 = d1 + charspeed * (passed / 1000)
             delta_d = d2 - d1
-            delta_pixel = int(d2*10) - int(d1*10)
+            delta_pixel = int(d2 * 10) - int(d1 * 10)
 
             CoreGame.distance += delta_d
             CoreGame.time += passed
 
+            # Mise à jour de l'affichage de la vitesse
             CoreGame.vitesseobj.text = str(int(charspeed * 3.6)) + " km/h"
+
+            # TODO: La barre ne change pas encore de taille mais déjà de couleur
+            # Mise à jour de l'affichage de la barre d'énergie
+            LARGEUR = -4
+            HAUTEUR = -4
+            POSITION_X = 2
+            POSITION_Y = 2
+            SCALE_X = 0
+            SCALE_Y = 0
+            SCALE_WIDTH = 1
+            SCALE_HEIGHT = 1
+            energy = CoreGame.characters_sprite[0].energy
+            if energy >= 70:
+                COULEUR = constantes.GREEN
+            elif energy >= 30:
+                COULEUR = constantes.YELLOW
+            else:
+                COULEUR = constantes.RED
+            BORDURE = 0  # rempli
+
+            CoreGame.barre_energie_in = rect.Rect(CoreGame.barre_energie_out, POSITION_X, POSITION_Y, SCALE_X,
+                                                  SCALE_Y, LARGEUR, HAUTEUR, SCALE_WIDTH, SCALE_HEIGHT, COULEUR,
+                                                  BORDURE)
+
+            # Mise à jour des boutons à appuyer
             key.Key.updatekeys(passed)
 
             if CoreGame.modejeu == "400m":
                 temps_ms = CoreGame.time
-                aff_ms = str(temps_ms%1000)
-                temps_s = int(temps_ms//1000)
-                aff_s = temps_s%60
-                temps_min = int(temps_s//60)
+                aff_ms = str(temps_ms % 1000)
+                temps_s = int(temps_ms // 1000)
+                aff_s = temps_s % 60
+                temps_min = int(temps_s // 60)
 
                 if aff_s < 10:
                     aff_s = "0" + str(aff_s)
@@ -274,8 +310,16 @@ class CoreGame:
 
                 CoreGame.tempsobj.text = str(temps_min) + ":" + aff_s + "." + aff_ms
 
-            if random.randint(1,100) == 1 and key.Key.canCreateKey():  # arbitraire pour l'instant, car la chance augmente avec la distance parcouru
-                key.Key(CoreGame.surface_boutons, 10)  # timeout arbitraire pour l'instant, il dépend normalement de la difficulté
+            # On vérifie que l'on as pas atteint la distance nécessaire
+            if CoreGame.modejeu == "400m" or CoreGame.modejeu == "400m haie":
+                if CoreGame.distance >= 400:
+                    # temporaire
+                    CoreGame.pause = True
+
+            if random.randint(1,
+                              100) == 1 and key.Key.canCreateKey():  # arbitraire pour l'instant, car la chance augmente avec la distance parcouru
+                key.Key(CoreGame.surface_boutons,
+                        10)  # timeout arbitraire pour l'instant, il dépend normalement de la difficulté
 
             for decors in CoreGame.mapscript.getDecors():
                 for surface in decors:
@@ -294,11 +338,14 @@ class CoreGame:
                 character.state = new_state
 
                 # On charge le perso
-                character.__getattribute__(new_state + "sprite").next(-int(characterinfos[new_state]["framesize"][0]/2), -int(characterinfos[new_state]["framesize"][1]/2))
+                character.__getattribute__(new_state + "sprite").next(
+                    -int(characterinfos[new_state]["framesize"][0] / 2),
+                    -int(characterinfos[new_state]["framesize"][1] / 2))
 
     def keypressed(cls, pygame, event):
         if event.key == pygame.K_SPACE:
-            CoreGame.characters_sprite[0].jump()  # bon, ici on va faire sauter le joueur 1 ! Il faudra pendre en compte cela lors qu'on dera le mode 2 joueurs
+            CoreGame.characters_sprite[
+                0].jump()  # bon, ici on va faire sauter le joueur 1 ! Il faudra pendre en compte cela lors qu'on dera le mode 2 joueurs
         else:
             key.Key.keypressed(event.dict["unicode"].capitalize())
 
